baddb90202ac528c45b29ca2046fc248b9e062af
angcyo <angcyo@126.com>
2019-02-20 23:57:27 +0800

    add adapter status demo

--- demo/src/main/java/com/angcyo/uikitdemo/ui/MainFragment.kt
+++ demo/src/main/java/com/angcyo/uikitdemo/ui/MainFragment.kt
@@ -2,9 +2,11 @@ package com.angcyo.uikitdemo.ui
 
 import android.content.Context
 import android.os.Bundle
-import android.view.View
+import android.support.v4.app.Fragment
 import com.angcyo.uikitdemo.R
+import com.angcyo.uikitdemo.ui.demo.AdapterStatusDemo
 import com.angcyo.uiview.less.base.BaseItemFragment
+import com.angcyo.uiview.less.base.helper.FragmentHelper
 import com.angcyo.uiview.less.kotlin.getColor
 import com.angcyo.uiview.less.recycler.RBaseViewHolder
 import com.angcyo.uiview.less.recycler.item.Item
@@ -27,45 +29,46 @@ class MainFragment : BaseItemFragment() {
     override fun onCreateItems(singleItems: ArrayList<SingleItem>) {
         singleItems.add(object : MainItem(Type.TOP) {
             override fun onBindView(holder: RBaseViewHolder, posInData: Int, itemDataBean: Item?) {
-                initItem(holder, "测试", View.OnClickListener {
-
-                })
+                initItem(holder, "AdapterStatusDemo", AdapterStatusDemo::class.java)
             }
         })
         singleItems.add(object : MainItem(Type.LINE) {
             override fun onBindView(holder: RBaseViewHolder, posInData: Int, itemDataBean: Item?) {
-                initItem(holder, "测试", View.OnClickListener {
-
-                })
+                initItem(holder, "测试", AdapterStatusDemo::class.java)
             }
         })
     }
-}
 
-open class MainItem : SingleItem {
-    constructor() : super()
-    constructor(type: Type?, tag: String?) : super(type, tag)
-    constructor(tag: String?) : super(tag)
-    constructor(type: Type?) : super(type)
-    constructor(type: Type?, lineColor: Int) : super(type, lineColor)
-    constructor(context: Context?) : super(context)
-    constructor(topOffset: Int) : super(topOffset)
-    constructor(leftOffset: Int, topOffset: Int) : super(leftOffset, topOffset)
-    constructor(leftOffset: Int, topOffset: Int, lineColor: Int) : super(leftOffset, topOffset, lineColor)
+    open inner class MainItem : SingleItem {
+        constructor() : super()
+        constructor(type: Type?, tag: String?) : super(type, tag)
+        constructor(tag: String?) : super(tag)
+        constructor(type: Type?) : super(type)
+        constructor(type: Type?, lineColor: Int) : super(type, lineColor)
+        constructor(context: Context?) : super(context)
+        constructor(topOffset: Int) : super(topOffset)
+        constructor(leftOffset: Int, topOffset: Int) : super(leftOffset, topOffset)
+        constructor(leftOffset: Int, topOffset: Int, lineColor: Int) : super(leftOffset, topOffset, lineColor)
 
-    override fun getItemLayoutId(): Int {
-        return super.getItemLayoutId()
-    }
+        override fun getItemLayoutId(): Int {
+            return super.getItemLayoutId()
+        }
 
-    override fun onBindView(holder: RBaseViewHolder, posInData: Int, itemDataBean: Item?) {
-    }
+        override fun onBindView(holder: RBaseViewHolder, posInData: Int, itemDataBean: Item?) {
+        }
 
-    fun initItem(holder: RBaseViewHolder, text: String, listener: View.OnClickListener) {
-        holder.item(R.id.base_item_info_layout).apply {
-            setLeftDrawableRes(R.drawable.ic_logo_little)
-            setItemText(text)
+        fun initItem(holder: RBaseViewHolder, text: String, fragment: Class<out Fragment>? = null) {
+            holder.item(R.id.base_item_info_layout).apply {
+                setLeftDrawableRes(R.drawable.ic_logo_little)
+                setItemText(text)
 
-            holder.click(this, listener)
+                holder.click(this) {
+                    fragment?.let {
+                        FragmentHelper.build(parentFragmentManager()).showFragment(fragment).doIt()
+                    }
+                }
+            }
         }
     }
 }
+
---
+++ demo/src/main/java/com/angcyo/uikitdemo/ui/demo/AdapterStatusDemo.kt
@@ -0,0 +1,54 @@
+package com.angcyo.uikitdemo.ui.demo
+
+import android.os.Bundle
+import com.angcyo.uikitdemo.R
+import com.angcyo.uiview.less.base.BaseRecyclerFragment
+import com.angcyo.uiview.less.recycler.RBaseViewHolder
+import com.angcyo.uiview.less.recycler.adapter.RBaseAdapter
+import com.angcyo.uiview.less.recycler.widget.IShowState
+import com.angcyo.uiview.less.recycler.widget.ItemShowStateLayout
+import com.scwang.smartrefresh.layout.api.RefreshLayout
+
+/**
+ *
+ * Email:angcyo@126.com
+ * @author angcyo
+ * @date 2019/02/20
+ */
+
+class AdapterStatusDemo : BaseRecyclerFragment<String>() {
+
+    override fun getContentLayoutId(): Int {
+        return R.layout.fragment_adapter_status
+    }
+
+    override fun onInitBaseView(viewHolder: RBaseViewHolder, arguments: Bundle?, savedInstanceState: Bundle?) {
+        super.onInitBaseView(viewHolder, arguments, savedInstanceState)
+        val itemShowStateLayout: ItemShowStateLayout = viewHolder.v(R.id.show_state_layout)
+        viewHolder.click(R.id.empty_button) {
+            itemShowStateLayout.showState = IShowState.EMPTY
+            baseAdapter.setShowState(IShowState.EMPTY)
+        }
+        viewHolder.click(R.id.load_button) {
+            itemShowStateLayout.showState = IShowState.LOADING
+            baseAdapter.setShowState(IShowState.LOADING)
+        }
+        viewHolder.click(R.id.error_button) {
+            itemShowStateLayout.showState = IShowState.ERROR
+            baseAdapter.setShowState(IShowState.ERROR)
+        }
+        viewHolder.click(R.id.nonet_button) {
+            itemShowStateLayout.showState = IShowState.NONET
+            baseAdapter.setShowState(IShowState.NONET)
+        }
+    }
+
+    override fun onCreateAdapter(datas: MutableList<String>?): RBaseAdapter<String> {
+        return super.onCreateAdapter(datas)
+    }
+
+    override fun onBaseRefresh(refreshLayout: RefreshLayout?) {
+        super.onBaseRefresh(refreshLayout)
+        baseAdapter.appendData(mutableListOf("1", "2", "3"))
+    }
+}
