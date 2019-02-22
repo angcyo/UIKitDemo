21dc298198e374ba7df1b4f5c46fb2909c6dc5f7
angcyo <angcyo@126.com>
2019-02-22 10:51:57 +0800

    add tab layout demo

--- demo/src/main/java/com/angcyo/uikitdemo/ui/demo/TabLayoutDemo.kt
+++ demo/src/main/java/com/angcyo/uikitdemo/ui/demo/TabLayoutDemo.kt
@@ -5,6 +5,7 @@ import com.angcyo.uikitdemo.R
 import com.angcyo.uikitdemo.ui.base.AppBaseTitleFragment
 import com.angcyo.uikitdemo.ui.fragment.TextFragment
 import com.angcyo.uiview.less.recycler.RBaseViewHolder
+import com.angcyo.uiview.less.widget.group.RTabLayout
 import com.angcyo.uiview.less.widget.pager.RFragmentAdapter
 
 /**
@@ -23,20 +24,26 @@ class TabLayoutDemo : AppBaseTitleFragment() {
 
         viewHolder.rpager(R.id.view_pager).apply {
             adapter =
-                    RFragmentAdapter(
-                        childFragmentManager,
-                        listOf(
-                            TextFragment.get("Fragment 1"),
-                            TextFragment.get("Fragment 2"),
-                            TextFragment.get("Fragment 3"),
-                            TextFragment.get("Fragment 4")
-                        )
+                RFragmentAdapter(
+                    childFragmentManager,
+                    listOf(
+                        TextFragment.get("Fragment 1"),
+                        TextFragment.get("Fragment 2"),
+                        TextFragment.get("Fragment 3"),
+                        TextFragment.get("Fragment 4"),
+                        TextFragment.get("Fragment 5"),
+                        TextFragment.get("Fragment 6"),
+                        TextFragment.get("Fragment 7"),
+                        TextFragment.get("Fragment 8")
                     )
-                        .apply {
-                            setParentFragment(this@TabLayoutDemo)
-                        }
+                )
+                    .apply {
+                        setParentFragment(this@TabLayoutDemo)
+                    }
 
             viewHolder.tab(R.id.tab8).setupViewPager(this)
+            viewHolder.tab(R.id.tab9).setupViewPager(this)
+            viewHolder.tab(R.id.tab9).onTabLayoutListener = RTabLayout.DefaultTabLayoutListener(this)
         }
     }
 }
