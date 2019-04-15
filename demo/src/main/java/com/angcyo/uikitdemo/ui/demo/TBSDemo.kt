d5f7893d758a026762ed18d2b71df5cdaf2eff83
angcyo <angcyo@126.com>
2019-04-15 17:18:01 +0800

    add tbs demo

--- demo/src/main/java/com/angcyo/uikitdemo/ui/demo/TBSDemo.kt
+++ demo/src/main/java/com/angcyo/uikitdemo/ui/demo/TBSDemo.kt
@@ -2,11 +2,17 @@ package com.angcyo.uikitdemo.ui.demo
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.LinearLayout
+import com.angcyo.lib.L
 import com.angcyo.rtbs.X5WebView
 import com.angcyo.uikitdemo.R
 import com.angcyo.uiview.less.base.BaseTitleFragment
+import com.angcyo.uiview.less.kotlin.extName
 import com.angcyo.uiview.less.recycler.RBaseViewHolder
+import com.angcyo.uiview.less.utils.Root
+import com.tencent.smtt.sdk.TbsReaderView
+
 
 /**
  *
@@ -19,7 +25,11 @@ class TBSDemo : BaseTitleFragment() {
 
     var webView: X5WebView? = null
 
+    var tbsReaderView: TbsReaderView? = null
+
     val DEFAULT_URL = "http://www.baidu.com"
+
+    val DEFAULT_FILE_PATH = "/sdcard/word.doc"
 
     override fun getContentLayoutId(): Int {
         return R.layout.item_tbs_layout
@@ -31,20 +41,48 @@ class TBSDemo : BaseTitleFragment() {
         viewHolder.group(R.id.layout).apply {
             if (childCount <= 2) {
                 webView = X5WebView(mAttachContext)
+
                 addView(webView, LinearLayout.LayoutParams(-1, 0, 1f))
-                //addView(webView, LinearLayout.LayoutParams(-1, 0, 1f))
 
                 webView?.loadUrl(DEFAULT_URL)
 
                 viewHolder.exV(R.id.url_edit).setInputText(DEFAULT_URL)
+                viewHolder.exV(R.id.file_edit).setInputText(DEFAULT_FILE_PATH)
+
+                openFile(this, DEFAULT_FILE_PATH)
             }
         }
 
         viewHolder.click(R.id.go) {
             webView?.loadUrl(viewHolder.exV(R.id.url_edit).string())
         }
+
+        viewHolder.click(R.id.open_file) {
+            openFile(viewHolder.group(R.id.layout), viewHolder.exV(R.id.file_edit).string())
+        }
     }
 
+    fun openFile(parent: ViewGroup, filePath: String) {
+        if (parent.childCount > 3) {
+            parent.removeViewAt(3)
+        }
+        tbsReaderView?.onStop()
+        tbsReaderView = TbsReaderView(mAttachContext,
+            TbsReaderView.ReaderCallback { p0, p1, p2 -> L.w("文件浏览服务日志输出:$p0 $p1 $p2") })
+        parent.addView(tbsReaderView, LinearLayout.LayoutParams(-1, 0, 1f))
+        openFile(filePath)
+    }
+
+    fun openFile(filePath: String) {
+        if (tbsReaderView?.preOpen(filePath.extName(), false) == true) {
+            val bundle = Bundle()
+            bundle.putString("filePath", filePath)
+            bundle.putString("tempPath", Root.sd())
+            tbsReaderView?.openFile(bundle)
+        } else {
+            L.e("不支持文件:$filePath")
+        }
+    }
 
     override fun onFragmentShow(bundle: Bundle?) {
         super.onFragmentShow(bundle)
@@ -63,6 +101,7 @@ class TBSDemo : BaseTitleFragment() {
     override fun onDestroy() {
         super.onDestroy()
         webView?.destroy()
+        tbsReaderView?.onStop()
     }
 
     override fun onBackPressed(activity: Activity): Boolean {
