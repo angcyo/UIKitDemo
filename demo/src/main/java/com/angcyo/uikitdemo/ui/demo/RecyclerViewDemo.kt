5345592802784a4da183736cdd121ded8dc24f41
angcyo <angcyo@126.com>
2019-05-09 16:00:49 +0800

    add linear / grid test

--- demo/src/main/java/com/angcyo/uikitdemo/ui/demo/RecyclerViewDemo.kt
+++ demo/src/main/java/com/angcyo/uikitdemo/ui/demo/RecyclerViewDemo.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.os.Bundle
 import com.angcyo.uikitdemo.R
 import com.angcyo.uikitdemo.ui.base.AppBaseTitleFragment
+import com.angcyo.uiview.less.kotlin.clearItemDecoration
 import com.angcyo.uiview.less.kotlin.dpi
 import com.angcyo.uiview.less.kotlin.dslAdapter
 import com.angcyo.uiview.less.kotlin.renderItem
@@ -24,13 +25,84 @@ class RecyclerViewDemo : AppBaseTitleFragment() {
         return R.layout.demo_recycler_view
     }
 
+
+    //分割线的位置
+    val overPositionList = mutableListOf<Int>()
+
     override fun onInitBaseView(viewHolder: RBaseViewHolder, arguments: Bundle?, savedInstanceState: Bundle?) {
         super.onInitBaseView(viewHolder, arguments, savedInstanceState)
+        gridLayoutTest()
 
-        //分割线的位置
-        val overPositionList = mutableListOf<Int>()
+        viewHolder.click(R.id.grid_button) {
+            gridLayoutTest()
+        }
 
-        viewHolder.rv(R.id.recycler_view).apply {
+        viewHolder.click(R.id.linear_button) {
+            linearLayoutTest()
+        }
+    }
+
+    fun gridLayoutTest() {
+        overPositionList.clear()
+
+        baseViewHolder.rv(R.id.recycler_view).apply {
+            clearItemDecoration()
+
+            dslAdapter(4) {
+                for (i in 0..2) {
+                    renderItem {
+                        itemLayoutId = R.layout.item_image_little
+                    }
+                }
+
+                for (i in 0..5) {
+                    overPositionList.add(i * 7 + 3)
+
+                    renderItem {
+                        itemSpanCount = 4
+
+                        itemLayoutId = R.layout.item_text
+
+                        itemBind = { itemHolder, itemPosition, adapterItem ->
+                            itemHolder.tv(R.id.text_view).text = "位置$itemPosition"
+
+                            itemHolder.clickItem {
+                                TopToast.show("点击位置:$itemPosition", -1)
+                            }
+
+                            itemHolder.click(R.id.check_box) {
+                                TopToast.show("CheckBox:$itemPosition", -1)
+                            }
+                        }
+                    }
+
+                    for (i in 0..5) {
+                        renderItem {
+                            itemLayoutId = R.layout.item_image_little
+                        }
+                    }
+                }
+            }
+
+            HoverItemDecoration().attachToRecyclerView(this) {
+                decorationOverLayoutType = {
+                    R.layout.item_text
+                }
+
+                haveOverDecoration = {
+                    overPositionList.contains(it)
+                }
+            }
+        }
+    }
+
+    fun linearLayoutTest() {
+        overPositionList.clear()
+
+        baseViewHolder.rv(R.id.recycler_view).apply {
+            resetLayoutManager(context, "V")
+            clearItemDecoration()
+
             dslAdapter {
                 for (i in 0..2) {
                     renderItem {
