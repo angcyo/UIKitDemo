69d808901d423287d4821201b3975d834bb5961f
angcyo <angcyo@126.com>
2019-06-15 15:11:28 +0800

    * fix tbs demo

--- demo/src/main/java/com/angcyo/uikitdemo/ui/demo/TBSDemo.kt
+++ demo/src/main/java/com/angcyo/uikitdemo/ui/demo/TBSDemo.kt
@@ -4,11 +4,14 @@ import android.app.Activity
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.TextView
 import com.angcyo.lib.L
+import com.angcyo.okdownload.FDown
 import com.angcyo.rtbs.X5WebView
+import com.angcyo.rtbs.fragment.FilePreviewFragment
 import com.angcyo.uikitdemo.R
 import com.angcyo.uiview.less.base.BaseTitleFragment
-import com.angcyo.uiview.less.kotlin.extName
+import com.angcyo.uiview.less.kotlin.*
 import com.angcyo.uiview.less.recycler.RBaseViewHolder
 import com.angcyo.uiview.less.utils.Root
 import com.tencent.smtt.sdk.TbsReaderView
@@ -38,6 +41,16 @@ class TBSDemo : BaseTitleFragment() {
     override fun onInitBaseView(viewHolder: RBaseViewHolder, arguments: Bundle?, savedInstanceState: Bundle?) {
         super.onInitBaseView(viewHolder, arguments, savedInstanceState)
 
+        viewHolder.auto(
+            R.id.file_edit,
+            mutableListOf(
+                "http://lc-orltc1ed.cn-n1.lcfile.com/a2b7a1c668e304cb7300/Effective%20Java%20%E4%B8%AD%E6%96%87%E7%AC%AC%E4%BA%8C%E7%89%88.pdf".decode(),
+                "http://mfiles.sohu.com/news/shanghetuzzx/14.jpg".decode(),
+                "http://mfiles.sohu.com/news/shanghetuzzx/15.jpg".decode(),
+                "http://www.duwenzhang.com/wenzhang/aiqingwenzhang/shanggan/20190604/403685.html".decode()
+            )
+        )
+
         viewHolder.group(R.id.layout).apply {
             if (childCount <= 2) {
                 webView = X5WebView(mAttachContext)
@@ -46,20 +59,41 @@ class TBSDemo : BaseTitleFragment() {
 
                 webView?.loadUrl(DEFAULT_URL)
 
-                viewHolder.exV(R.id.url_edit).setInputText(DEFAULT_URL)
-                viewHolder.exV(R.id.file_edit).setInputText(DEFAULT_FILE_PATH)
+                viewHolder.eV(R.id.url_edit).setInputText(DEFAULT_URL)
+                viewHolder.eV(R.id.file_edit).setInputText(DEFAULT_FILE_PATH)
 
-                openFile(this, DEFAULT_FILE_PATH)
+                //openFile(this, DEFAULT_FILE_PATH)
             }
         }
 
         viewHolder.click(R.id.go) {
-            webView?.loadUrl(viewHolder.exV(R.id.url_edit).string())
+            webView?.loadUrl(viewHolder.eV(R.id.url_edit).string())
         }
 
         viewHolder.click(R.id.open_file) {
-            openFile(viewHolder.group(R.id.layout), viewHolder.exV(R.id.file_edit).string())
+            openFile(viewHolder.group(R.id.layout), viewHolder.eV(R.id.file_edit).string())
         }
+
+        viewHolder.click(R.id.mine_type) {
+            (it as? TextView)?.text = viewHolder.eV(R.id.file_edit).string().mimeType()
+        }
+
+        viewHolder.click(R.id.open_file_new) {
+            FilePreviewFragment.show(wtf()) {
+                filePath = viewHolder.eV(R.id.file_edit).string()
+            }
+        }
+        viewHolder.click(R.id.open_url_new) {
+            FilePreviewFragment.openUrl(wtf()) {
+                targetUrl = viewHolder.ev(R.id.url_edit).string()
+            }
+        }
+
+        FDown.cancel(0)
+        FDown.cancel(-1)
+        FDown.cancel(-2)
+        FDown.cancel("")
+        FDown.cancel(null)
     }
 
     fun openFile(parent: ViewGroup, filePath: String) {
