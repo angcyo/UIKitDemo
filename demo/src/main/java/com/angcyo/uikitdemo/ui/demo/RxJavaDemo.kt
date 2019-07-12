88b4d7807c8089755dd124f3152626983cfc3870
angcyo <angcyo@126.com>
2019-07-12 15:24:41 +0800

    * fix demo

--- demo/src/main/java/com/angcyo/uikitdemo/ui/demo/RxJavaDemo.kt
+++ demo/src/main/java/com/angcyo/uikitdemo/ui/demo/RxJavaDemo.kt
@@ -1,14 +1,17 @@
 package com.angcyo.uikitdemo.ui.demo
 
+import android.os.Bundle
+import com.angcyo.http.Func
 import com.angcyo.http.HttpSubscriber
 import com.angcyo.http.Rx
 import com.angcyo.lib.L
 import com.angcyo.uikitdemo.R
-import com.angcyo.uiview.less.base.BaseItemFragment
+import com.angcyo.uikitdemo.ui.base.AppBaseDslRecyclerFragment
+import com.angcyo.uiview.less.kotlin.renderItem
 import com.angcyo.uiview.less.recycler.RBaseViewHolder
-import com.angcyo.uiview.less.recycler.item.Item
-import com.angcyo.uiview.less.recycler.item.SingleItem
-import java.util.*
+import com.angcyo.uiview.less.utils.NetStateChangeObserver
+import com.angcyo.uiview.less.utils.NetworkType
+import com.angcyo.uiview.less.utils.RNetwork
 
 /**
  *
@@ -16,51 +19,118 @@ import java.util.*
  * @author angcyo
  * @date 2019/03/09
  */
-class RxJavaDemo : BaseItemFragment() {
-    override fun onCreateItems(singleItems: ArrayList<SingleItem>) {
-        singleItems.add(object : SingleItem() {
-            override fun onBindView(holder: RBaseViewHolder, posInData: Int, itemDataBean: Item?) {
-                holder.item(R.id.base_item_info_layout).setItemText("operators")
+class RxJavaDemo : AppBaseDslRecyclerFragment() {
+    val observer: NetStateChangeObserver by lazy {
+        object : NetStateChangeObserver {
+            override fun onNetDisconnected() {
+                L.e("RxJavaDemo:断网")
 
-                //http://reactivex.io/documentation/operators.html
-                holder.clickItem {
-                    Rx.merge<List<Int>>(
-                        Rx.just(1).map {
-                            listOf(it)
-                        },
-                        Rx.just(2).map {
-                            listOf(it)
-                        },
-                        Rx.just(5).map {
-                            listOf(it)
-                        },
-                        Rx.just(7).map {
-                            listOf(it)
+                renderDslAdapter {
+                    renderItem {
+                        itemLayoutId = R.layout.base_item_info_layout
+                        itemBind = { itemHolder, _, _ ->
+                            itemHolder.item(R.id.base_item_info_layout).setItemText("RxJavaDemo:断网")
                         }
-                    )
-                        .map {
-                            it
-                        }
-                        .scan { t1: List<Int>?, t2: List<Int>? ->
-                            val result = mutableListOf<Int>()
-                            t1?.let {
-                                result.addAll(it)
-                            }
-                            t2?.let {
-                                result.addAll(it)
-                            }
-                            result
-                        }
-                        .subscribe(object : HttpSubscriber<List<Int>>() {
-                            override fun onEnd(data: List<Int>?, error: Throwable?) {
-                                super.onEnd(data, error)
-                                L.i(data)
-                            }
-                        })
+                    }
                 }
             }
 
-        })
+            override fun onNetConnected(networkType: NetworkType) {
+                L.w("RxJavaDemo:网络->$networkType")
+
+                renderDslAdapter {
+                    renderItem {
+                        itemLayoutId = R.layout.base_item_info_layout
+                        itemBind = { itemHolder, _, _ ->
+                            itemHolder.item(R.id.base_item_info_layout).setItemText("RxJavaDemo:网络->$networkType")
+                        }
+                    }
+                }
+            }
+        }
     }
 
+    override fun onFragmentShow(bundle: Bundle?) {
+        super.onFragmentShow(bundle)
+        RNetwork.registerObserver(observer)
+    }
+
+    override fun onFragmentHide() {
+        super.onFragmentHide()
+        RNetwork.unregisterObserver(observer)
+    }
+
+    override fun onInitBaseView(viewHolder: RBaseViewHolder, arguments: Bundle?, savedInstanceState: Bundle?) {
+        super.onInitBaseView(viewHolder, arguments, savedInstanceState)
+
+        renderDslAdapter {
+            renderItem {
+                itemLayoutId = R.layout.base_item_info_layout
+                itemBind = { itemHolder, _, _ ->
+
+                    itemHolder.item(R.id.base_item_info_layout).setItemText("operators")
+
+                    //http://reactivex.io/documentation/operators.html
+                    itemHolder.clickItem {
+                        Rx.merge<List<Int>>(
+                            Rx.just(1).map {
+                                listOf(it)
+                            },
+                            Rx.just(2).map {
+                                listOf(it)
+                            },
+                            Rx.just(5).map {
+                                listOf(it)
+                            },
+                            Rx.just(7).map {
+                                listOf(it)
+                            }
+                        )
+                            .map {
+                                it
+                            }
+                            .scan { t1: List<Int>?, t2: List<Int>? ->
+                                val result = mutableListOf<Int>()
+                                t1?.let {
+                                    result.addAll(it)
+                                }
+                                t2?.let {
+                                    result.addAll(it)
+                                }
+                                result
+                            }
+                            .subscribe(object : HttpSubscriber<List<Int>>() {
+                                override fun onEnd(data: List<Int>?, error: Throwable?) {
+                                    super.onEnd(data, error)
+                                    L.i(data)
+                                }
+                            })
+                    }
+                }
+            }
+
+            renderItem {
+                itemLayoutId = R.layout.base_item_info_layout
+                itemBind = { itemHolder, _, _ ->
+
+                    itemHolder.item(R.id.base_item_info_layout).setItemText("Rx Base Test")
+
+                    //http://reactivex.io/documentation/operators.html
+                    itemHolder.clickItem {
+                        Rx.back {
+                            L.i("onBack")
+                        }
+
+                        Rx.main {
+                            L.i("onMain")
+                        }
+
+                        Rx.create(Func {
+                            L.i("onCreate")
+                        }).subscribe()
+                    }
+                }
+            }
+        }
+    }
 }
