4c67f6a7d71c6f234186e819052c58e9a90aed16
angcyo <angcyo@126.com>
2019-09-03 10:21:23 +0800

    + message state test

--- demo/src/androidTest/java/com/angcyo/uikitdemo/kotlin/qq/MessageStateHelperTest.kt
+++ demo/src/androidTest/java/com/angcyo/uikitdemo/kotlin/qq/MessageStateHelperTest.kt
@@ -1,6 +1,7 @@
 package com.angcyo.uikitdemo.kotlin.qq
 
 import android.os.SystemClock
+import com.angcyo.lib.L
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -9,10 +10,10 @@ import org.junit.Test
  * Email:angcyo@126.com
  *
  * @author angcyo
- * @date 2019/09/02
+ * @date 2019/09/03
+ * Copyright (c) 2019 ShenZhen O&M Cloud Co., Ltd. All rights reserved.
  */
-class MessageStateHelperJavaTest {
-
+class MessageStateHelperTest {
     @Before
     fun setUp() {
     }
@@ -28,20 +29,31 @@ class MessageStateHelperJavaTest {
         MessageStateHelper.MESSAGE_TIMEOUT = 5 * 1000L
         MessageStateHelper.listeners.add(object : OnMessageStateListener {
             override fun onMessageStateChanged(msgId: Long, formState: Int, toState: Int) {
-                println(
-                    "消息状态改变:$msgId ${MessageStateHelper.messageStateString(formState)}->${MessageStateHelper.messageStateString(
-                        toState
-                    )}"
-                )
+                if (toState == MessageStateHelper.MESSAGE_STATE_TIMEOUT) {
+                    L.e(
+                        "消息超时:$msgId ${MessageStateHelper.messageStateString(formState)}->${MessageStateHelper.messageStateString(
+                            toState
+                        )}"
+                    )
+                } else {
+                    L.i(
+                        "消息状态:$msgId ${MessageStateHelper.messageStateString(formState)}->${MessageStateHelper.messageStateString(
+                            toState
+                        )}"
+                    )
+                }
             }
         })
         MessageStateHelper.start()
 
         for (l in 0..10L) {
             MessageStateHelper.updateMessageState(l, MessageStateHelper.MESSAGE_STATE_NORMAL)
+            SystemClock.sleep(2 * 1000L)
         }
 
         SystemClock.sleep(60 * 1000L)
+        MessageStateHelper.stop()
+
         println("${Thread.currentThread().name} ....test....end")
     }
 }
--- demo/src/main/java/com/angcyo/uikitdemo/kotlin/qq/MessageStateHelper.kt
+++ demo/src/main/java/com/angcyo/uikitdemo/kotlin/qq/MessageStateHelper.kt
@@ -17,7 +17,7 @@ object MessageStateHelper {
     /**消息状态改变回调, 主线程*/
     val listeners = mutableSetOf<OnMessageStateListener>()
 
-    private val messageStateMap = ConcurrentHashMap<Long, Int>()
+    private val messageStateMap = ConcurrentHashMap<Long, MessageData>()
 
     private var handler: Handler? = null
     private var handlerThread: HandlerThread? = null
@@ -30,6 +30,20 @@ object MessageStateHelper {
     const val MESSAGE_STATE_NORMAL = 1
     const val MESSAGE_STATE_FINISH = 2
     const val MESSAGE_STATE_TIMEOUT = 3
+
+    /**消息超时间隔时长, 60秒*/
+    var MESSAGE_TIMEOUT = 60 * 1000L
+    /**消息超时检查时间间隔*/
+    var MESSAGE_TIMEOUT_CHECK = 300L
+
+    fun messageStateString(state: Int): String {
+        return when (state) {
+            MESSAGE_STATE_NORMAL -> "MESSAGE_STATE_NORMAL"
+            MESSAGE_STATE_FINISH -> "MESSAGE_STATE_FINISH"
+            MESSAGE_STATE_TIMEOUT -> "MESSAGE_STATE_TIMEOUT"
+            else -> "MESSAGE_STATE_UNKNOWN"
+        }
+    }
 
     /**开始消息超时检查*/
     fun start() {
@@ -49,7 +63,9 @@ object MessageStateHelper {
                             } else if (from == to) {
                                 //状态相同的消息
                             } else {
-                                messageStateMap[msgId] = to
+                                val messageData = messageStateMap[msgId] ?: MessageData(msgId)
+                                messageData.state = to
+                                messageStateMap[msgId] = messageData
 
                                 //主线程回调
                                 mainHandler?.post {
@@ -65,11 +81,27 @@ object MessageStateHelper {
                             }
                         }
                         MESSAGE_CHECK_TIMEOUT -> {
+                            //检查消息是否超时
+                            messageStateMap.forEach { entry ->
+                                val messageData = entry.value
+                                if (messageData.state != MESSAGE_STATE_FINISH ||
+                                    messageData.state != MESSAGE_STATE_TIMEOUT
+                                ) {
+                                    val nowTime = System.currentTimeMillis()
+                                    if (nowTime - messageData.sendTime >= MESSAGE_TIMEOUT) {
+                                        //超时
+                                        updateMessageState(messageData.msgId, MESSAGE_STATE_TIMEOUT)
+                                    }
+                                }
+                            }
 
+                            checkMessageTimeout()
                         }
                     }
                     true
                 }
+
+                checkMessageTimeout()
             }
         }
         if (mainHandler == null) {
@@ -79,13 +111,16 @@ object MessageStateHelper {
 
     /**停止*/
     fun stop() {
+        handler?.removeMessages(MESSAGE_UPDATE_STATE)
+        handler?.removeMessages(MESSAGE_CHECK_TIMEOUT)
         handlerThread?.quit()
+        handler = null
         handlerThread = null
     }
 
     /**获取已缓存的消息的状态*/
     fun getMessageState(msgId: Long): Int {
-        return messageStateMap[msgId] ?: MESSAGE_STATE_UNKNOWN
+        return messageStateMap[msgId]?.state ?: MESSAGE_STATE_UNKNOWN
     }
 
     /**更新消息状态*/
@@ -95,6 +130,12 @@ object MessageStateHelper {
         }
         handler?.run {
             sendMessage(obtainMessage(MESSAGE_UPDATE_STATE, getMessageState(msgId), statue, msgId))
+        }
+    }
+
+    private fun checkMessageTimeout(delayMillis: Long = MESSAGE_TIMEOUT_CHECK) {
+        handler?.run {
+            sendMessageDelayed(obtainMessage(MESSAGE_CHECK_TIMEOUT), delayMillis)
         }
     }
 }
@@ -107,6 +148,12 @@ object MessageStateHelper {
         MessageStateHelper.MESSAGE_STATE_TIMEOUT]
 )
 annotation class MessageState {}
+
+private data class MessageData(
+    var msgId: Long,
+    @MessageState var state: Int = MessageStateHelper.MESSAGE_STATE_UNKNOWN,
+    var sendTime: Long = System.currentTimeMillis()
+)
 
 interface OnMessageStateListener {
     fun onMessageStateChanged(msgId: Long, formState: Int, toState: Int)
