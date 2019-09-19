dbc6e6e84b98c38a1fd6cbc0ea58ded85703a0b3
angcyo <angcyo@126.com>
2019-09-19 15:59:25 +0800

    * fix regular demo

--- demo/src/main/java/com/angcyo/uikitdemo/ui/demo/RegularPatternDemo.kt
+++ demo/src/main/java/com/angcyo/uikitdemo/ui/demo/RegularPatternDemo.kt
@@ -14,26 +14,35 @@ import com.angcyo.uiview.less.utils.*
  * @date 2019/06/16
  */
 class RegularPatternDemo : AppBaseDslRecyclerFragment() {
-    override fun onInitBaseView(viewHolder: RBaseViewHolder, arguments: Bundle?, savedInstanceState: Bundle?) {
+
+    init {
+        //默认值
+        "pattern_edit".hawkPutList(PATTERN_MOBILE_SIMPLE, false)
+        "pattern_edit".hawkPutList(PATTERN_MOBILE_EXACT, false)
+        "pattern_edit".hawkPutList(PATTERN_TEL, false)
+        "pattern_edit".hawkPutList(PATTERN_EMAIL, false)
+        "pattern_edit".hawkPutList(PATTERN_URL, false)
+        "pattern_edit".hawkPutList("(?<=compile).*(?=JavaWithJavac)", false)
+
+        "content_edit".hawkPutList("13678953476", false)
+        "content_edit".hawkPutList("compile_devDebugJavaWithJavac", false)
+    }
+
+    override fun onInitBaseView(
+        viewHolder: RBaseViewHolder,
+        arguments: Bundle?,
+        savedInstanceState: Bundle?
+    ) {
         super.onInitBaseView(viewHolder, arguments, savedInstanceState)
         baseDslAdapter.apply {
             renderItem {
                 itemLayoutId = R.layout.demo_regular_pattern_layout
 
                 itemBind = { itemHolder, _, _ ->
-                    itemHolder.auto(
-                        R.id.pattern_edit,
-                        mutableListOf(
-                            PATTERN_MOBILE_SIMPLE,
-                            PATTERN_MOBILE_EXACT,
-                            PATTERN_TEL,
-                            PATTERN_EMAIL,
-                            PATTERN_URL
-                        ),
-                        true
-                    )
-
-                    itemHolder.ev(R.id.content_edit).setInputText("13678953476")
+                    itemHolder.auto(R.id.pattern_edit, "pattern_edit".hawkGetList(), true)
+                        ?.setInputText("pattern_edit".hawkGetList().first())
+                    itemHolder.auto(R.id.content_edit, "content_edit".hawkGetList(), true)
+                        ?.setInputText("content_edit".hawkGetList().first())
 
                     itemHolder.click(R.id.matcher_button) {
                         val regexList = if (itemHolder.ev(R.id.pattern_edit).isEmpty()) {
@@ -45,15 +54,20 @@ class RegularPatternDemo : AppBaseDslRecyclerFragment() {
                         val contentString = itemHolder.ev(R.id.content_edit).string()
                         val pattern = contentString.pattern(regexList, false)
 
+                        "pattern_edit".hawkPutList(regexList.first())
+                        "content_edit".hawkPutList(contentString)
+
                         itemHolder.tv(R.id.text_view).text = RSpan
-                            .get("结果:$pattern")
+                            .get("匹配结果:$pattern")
                             .appendLine().apply {
                                 contentString.patternList(regexList.first()).forEach {
+                                    append("matcher.find()->matcher.group()")
+                                    appendLine()
                                     append(it)
                                     appendLine()
                                 }
                             }
-                            .append("--")
+                            .append("--end")
                             .create()
                     }
                 }
